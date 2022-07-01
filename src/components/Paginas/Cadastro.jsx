92231f0363de16478f17acd732219388519376c6
Jord Bousquet <jordbousquet@yahoo.com.br>
2022-07-01 14:26:43 -0300

    Cadastro de Serviços

--- src/components/Paginas/Cadastro.jsx
+++ src/components/Paginas/Cadastro.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
+import { BASE_URL } from '../../constants/urls'
+import { key } from '../../constants/key'
+
 
 export const Container = styled.div`
     display: flex;
@@ -35,22 +39,95 @@ export const BotaoCastrar = styled.button`
 `
 
 export default class Cadastro extends Component {
+
+  state = {
+
+    titulo: "",
+    descricao: "",
+    preco: "",
+    pagamento: [],
+    data: ""
+  }
+
+  pegaTitulo = (e) => {
+    this.setState({ titulo: e.target.value })
+  }
+
+  pegaDescrição = (e) => {
+    this.setState({ descricao: e.target.value })
+  }
+
+  pegaPreço = (e) => {
+    this.setState({ preco: e.target.value })
+  }
+
+  pegaData = (e) => {
+    this.setState({ data: e.target.value })
+  }
+
+  pegaPagamento = (e) => {
+    const value = Array.from(e.target.selectedOptions, option => option.value)
+    this.setState({ pagamento: value })
+  }
+
+  CadastrarServico = () => {
+    const body = {
+      title: this.state.titulo,
+      description: this.state.descricao,
+      price: Number(this.state.preco),
+      paymentMethods: this.state.pagamento,
+      dueDate: this.state.data
+
+    }
+
+    axios.post(`${BASE_URL}/jobs`, body, {
+      headers: {
+        Authorization: `${key}`
+      }
+    }).then((resp) => {
+      alert(`O serviço ${this.state.titulo} foi criado com sucesso!`)
+      this.setState({
+        titulo: "",
+        descricao: "",
+        preco: "",
+        pagamento: [],
+        data: ""
+
+      })
+
+    }).catch((err) => {
+      alert("Erro ao Cadastrar Serviço")
+    })
+  }
+
+
+
   render() {
     return (
       <Container>
         <TituloCadastro>Cadastre o seu serviço</TituloCadastro>
-        <InputCadastro placeholder ='Titulo'/>
-        <InputCadastro placeholder ='Descrição'/>
-        <InputCadastro placeholder ='Preço' type ='number'/>
-        <SelecCadastro multiple>
+        <InputCadastro placeholder='Titulo'
+          value={this.state.titulo}
+          onChange={this.pegaTitulo} />
+        <InputCadastro placeholder='Descrição'
+          value={this.state.descricao}
+          onChange={this.pegaDescrição} />
+        <InputCadastro placeholder='Preço' type='number'
+          value={this.state.preco}
+          onChange={this.pegaPreço} />
+        <SelecCadastro multiple
+          value={this.state.pagamento} onChange={this.pegaPagamento}>
           <option>Cartão de Debito</option>
           <option>Cartão de Crédito</option>
           <option>Paypal</option>
           <option>Boleto</option>
           <option>Pix</option>
         </SelecCadastro>
-        <InputCadastro placeholder ='Prazo do Serviço' type ='date'/>
-        <BotaoCastrar>Cadastrar</BotaoCastrar>
+
+        <InputCadastro placeholder='Prazo do Serviço' type='date'
+          value={this.state.data}
+          onChange={this.pegaData} />
+        <BotaoCastrar onClick={this.CadastrarServico}>Cadastrar</BotaoCastrar>
       </Container>
     )
   }
